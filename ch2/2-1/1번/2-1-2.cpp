c6212f504768288ca564a6e835bf9641fc8d8428
Junyeon-Kim713 <a84842326@gmail.com>
2026-03-13 00:29:49 +0900

    과제 업로드

---
+++ ch2/2-1/1번/2-1-2.cpp
@@ -0,0 +1,16 @@
+﻿/*
+제목 : cout을 이용해 간단한 문장들을 출력하는 프로그램
+저자 : 2301387 김준연
+날짜 : 26.03.12
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "이름 : 홍길동\n";
+	cout << "주소 : 군산시 대학로 558\n";
+	cout << "학번 : 201012\n";
+	cout << "차종 : 벤츠\n";
+	return 0;
+}
