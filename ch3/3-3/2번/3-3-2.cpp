42f91f38150d40752d6bff17dc061483dceb1ba5
Junyeon-Kim713 <a84842326@gmail.com>
2026-03-25 00:49:25 +0900

    파일 추가

---
+++ ch3/3-3/2번/3-3-2.cpp
@@ -0,0 +1,34 @@
+﻿// Title : 생성자와 소멸자를 가진 삼각형 클래스를 만들어 다양한 동작을 수행하는 프로그램
+// Author : 김준연
+// Date : 26.03.24
+
+#include <iostream> 
+using namespace std; 
+
+class Triangle {
+public:
+	int width, height;
+	Triangle();
+	Triangle(int a, int b);
+	~Triangle();
+	double getArea();
+};
+Triangle::Triangle() : Triangle(1,1) {}
+Triangle::Triangle(int a, int b) : width(a), height(b)
+{
+	cout << "밑변 " << width << "높이 " << height << "인 삼각형 생성" << endl;
+}
+Triangle::~Triangle() {
+	cout << "밑변 " << width << "높이 " << height << "인 삼각형 소멸" << endl;
+}
+double Triangle::getArea() {
+	return 0.5 * width * height;
+}
+int main() 
+{
+	Triangle tri1;
+	cout << "삼각형의 면적은 " << tri1.getArea() << endl;
+		Triangle tri2(2, 4); 
+		cout << "삼각형의 면적은 " << tri2.getArea() << endl;
+		return 0;
+}
