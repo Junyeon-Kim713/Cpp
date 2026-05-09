070e9246d58206387e685042c31dfb149a590f83
Junyeon-Kim713 <a84842326@gmail.com>
2026-05-09 23:26:05 +0900

    파일 추가

---
+++ ch5/5-1/4번/5-1-4.cpp
@@ -0,0 +1,23 @@
+﻿// Title : 주소에 의한 호출로 string 객체의 문자열을 교환하는 프로그램
+// Author : 김준연
+// Date : 26.05.09
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void SwapString(string* s1, string* s2);
+int main() {
+	string s1("hello");
+	string s2("world");
+	cout << "호출전 s1 : " << s1 << ", s2 : " << s2 << endl;
+	SwapString(&s1, &s2); 
+	cout << "호출후 s1 : " << s1 << ", s2 : " << s2 << endl;
+}
+void SwapString(string* s1, string* s2)
+{
+	string temp;
+	temp = *s1;
+	*s1 = *s2;
+	*s2 = temp;
+}
---
+++ ch5/5-1/5번/5-1-5.cpp
@@ -0,0 +1,28 @@
+﻿// Title : string 객체 배열을 인자로 전달해 사전편찬순서 상 가장 뒤에 나오는 문자열을 찾는 프로그램
+// Author : 김준연
+// Date : 26.05.09
+
+#include <iostream>
+#include <string>
+using namespace std;
+string GetMaxString(string* str);
+int main() {
+	string names[5];
+	string res;
+	for (int i = 0; i < 5; i++) {
+		cout << "이름 >> ";
+		getline(cin, names[i], '\n');
+	}
+	res = GetMaxString(names);
+	cout << "사전에서 가장 뒤에 나오는 문자열은 " << res << endl;
+}
+string GetMaxString(string* str)
+{
+	string max = str[0];
+	for (int i = 1; i < 5; i++)
+	{
+		if (max < str[i])
+			max = str[i];
+	}
+	return max;
+}
