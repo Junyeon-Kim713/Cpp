31b99e32094c680f30bbd56014ae3332c37fb8e6
Junyeon <a84842326@gmail.com>
2026-06-01 10:43:11 +0900

    Implement Complex class with operator overloading for +

---
+++ ch7/7-2/1번/7-2-1.cpp
@@ -0,0 +1,35 @@
+// Title : + 연산자 중복으로 복소수 객체의 +연산을 수행하는 프로그램
+// Author : 김준연
+// Date : 26.06.01
+
+#include <iostream>
+using namespace std;
+
+class Complex {
+	int real;
+	int img;
+public:
+	Complex(int r = 0, int i = 0) : real(r), img(i) {}
+	void show() {
+		if(img>0)
+			cout << real << '+' << img << 'i' << endl;
+		else
+			cout << real << img << 'i' << endl;
+	}
+	Complex  operator +(Complex& op2);
+
+};
+Complex Complex::operator +(Complex& op2) {
+	Complex temp;
+	temp.real = this->real + op2.real;
+	temp.img = this->img + op2.img;
+	return temp;
+	
+}
+int main() {
+	Complex x(2, 3), y(-5, 10), sum;
+	sum = x + y;
+	cout << "두 복소수의 합은";
+	sum.show();
+	return 0;
+}
